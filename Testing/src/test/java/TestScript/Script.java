66daac29ba46b62c86cd286c2b9e84e5b66fa166
PavanK <PavanK@OTSPUNL0005>
2023-02-17 16:30:10 +0530

    new change

--- Testing/src/test/java/TestScript/Script.java
+++ Testing/src/test/java/TestScript/Script.java
@@ -1,14 +1,24 @@
 package TestScript;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 public class Script extends Generic_Browser 
 {
 	@Test
-	public void Gmail()
+	public void Demo1()
 	{
 		System.out.println("Pipeline deployment is done working fine");
 	}
-
+	@Test
+	public void Demo2()
+	{
+		Assert.assertTrue(false);
+	}
+	@Test
+	public void Demo3()
+	{
+		Assert.assertTrue(true);
+	}
 }
